fab138fda21449389d3ea91c2088856754f3d0dd
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-20 10:22:38 +0400

    Chapters divided in classed functions. Reading, writing, and looping thought the array implemented.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Arrays101.DVD;
 
 public class Main {
     public static void main(String[] args) {
-        // The actual cole for creating an Array to hold DVD's.
-        DVD[] dvdCollection = new DVD[15];
+        DVD.whatIsAnArray();
+        DVD.accessingElementInArrays();
     }
 }
