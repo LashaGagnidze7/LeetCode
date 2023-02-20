ecc27aec6fbe952ff336df55ca2f19c5bbbda485
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-20 10:46:23 +0400

    Array Introduction finished. Getting ready for solving problems.

--- src/Arrays101/DVD.java
+++ src/Arrays101/DVD.java
@@ -100,4 +100,35 @@ public class DVD {
         }
         // Prints exactly the same as the previous example.
     }
+
+    public static void arrayCapacityVsLength() {
+        // Create a new array with a capacity of 6.
+        int[] array = new int[6];
+
+        // Current length is 0, because it has 0 elements.
+        int length = 0;
+
+        // Add 3 items into it.
+        for (int i = 0; i < 3; i++) {
+            array[i] = i * i;
+            // Each time we add an element, the length goes up by one.
+            length++;
+        }
+
+        System.out.println("The Array has a capacity of " + array.length);
+        System.out.println("The Array has a length of " + length);
+
+        // *** Handling Array Parameters ***
+        class Solution {
+            public int findMaxConsecutiveOnes(int[] nums) {
+                // Hint: Initialise and declare a variable here to
+                // keep track of how many 1's you've seen in a row.
+                for (int i = 0; i < nums.length; i++) {
+                    // Do something with element nums[i].
+                }
+
+                return 0; // For error-handling
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import Arrays101.DVD;
 
 public class Main {
     public static void main(String[] args) {
-        DVD.whatIsAnArray();
-        DVD.accessingElementInArrays();
+        // DVD.whatIsAnArray();
+        // DVD.accessingElementInArrays();
+        DVD.arrayCapacityVsLength();
     }
 }
